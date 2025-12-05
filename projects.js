7085d4040d271713ab5fadff86cc08760193ae98
Andre Lew <me@aelew.com>
2025-12-04 21:43:23 -0800

    fix: update localStorage load impl

--- projects.js
+++ projects.js
@@ -57,6 +57,49 @@ document.getElementById('load-remote')?.addEventListener('click', () => {
         throw new Error('Invalid data');
       }
       updateProjects(projects);
-      localStorage.setItem('projects', JSON.stringify(projects));
     });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  localStorage.setItem(
+    'projects',
+    JSON.stringify([
+      {
+        name: 'DevTerms',
+        description: 'Online dictionary curated for developers',
+        url: 'https://devterms.com',
+        cover: {
+          base: 'https://cse-134b-hw5.vercel.app/media/projects/devterms.jpg',
+          lg: 'https://cse-134b-hw5.vercel.app/media/projects/devterms-lg.jpg',
+        },
+      },
+      {
+        name: 'Cobalt',
+        description: 'Social media downloader extension for Raycast',
+        url: 'https://www.raycast.com/aelew/cobalt',
+        cover: {
+          base: 'https://cse-134b-hw5.vercel.app/media/projects/raycast-cobalt.jpg',
+          lg: 'https://cse-134b-hw5.vercel.app/media/projects/raycast-cobalt-lg.jpg',
+        },
+      },
+      {
+        name: 'Mailery',
+        description: 'Cross-platform email client',
+        url: 'https://mailery.app',
+        cover: {
+          base: 'https://cse-134b-hw5.vercel.app/media/projects/mailery.jpg',
+          lg: 'https://cse-134b-hw5.vercel.app/media/projects/mailery-lg.jpg',
+        },
+      },
+      {
+        name: 'Tech Internship Alerts',
+        description: 'Job listing and monitoring bot',
+        url: 'https://github.com/aelew/tech-internship-alerts',
+        cover: {
+          base: 'https://cse-134b-hw5.vercel.app/media/projects/tech-internship-alerts.jpg',
+          lg: 'https://cse-134b-hw5.vercel.app/media/projects/tech-internship-alerts-lg.jpg',
+        },
+      },
+    ])
+  );
 });
