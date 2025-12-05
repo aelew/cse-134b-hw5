304dc1228c301cc6af82dbb72856e4be18496434
Andre Lew <me@aelew.com>
2025-12-04 21:19:26 -0800

    feat: start adding project card custom element

---
+++ projects.js
@@ -0,0 +1,64 @@
+class ProjectCardElement extends HTMLElement {
+  project;
+
+  constructor(project) {
+    super();
+    this.project = project;
+  }
+
+  connectedCallback() {
+    if (!this.project) {
+      return;
+    }
+
+    this.innerHTML = `
+          <project-card>
+            <project-card-header>
+              <h3>${this.project.name}</h3>
+              <p>${this.project.description}</p>
+            </project-card-header>
+
+            <picture>
+              <source srcset="${this.project.cover.lg}" media="(min-width: 768px)" />
+              <img
+                alt="Cover for ${this.project.name} project"
+                src="${this.project.cover.base}"
+                height="173"
+                width="330"
+              />
+            </picture>
+
+            <a href="${this.project.url}" target="_blank">View project</a>
+          </project-card>
+    `;
+  }
+}
+
+customElements.define('project-card', ProjectCardElement);
+
+function updateProjects(projects) {
+  projects.forEach((project) => {
+    const card = new ProjectCardElement(project);
+    document.getElementById('project-grid')?.appendChild(card);
+  });
+}
+
+document.getElementById('load-local')?.addEventListener('click', () => {
+  const projects = JSON.parse(localStorage.getItem('projects'));
+  if (!Array.isArray(projects)) {
+    throw new Error('Invalid data');
+  }
+  updateProjects(projects);
+});
+
+document.getElementById('load-remote')?.addEventListener('click', () => {
+  fetch('https://my-json-server.typicode.com/aelew/cse-134b-hw5-data/projects')
+    .then((r) => r.json())
+    .then((projects) => {
+      if (!Array.isArray(projects)) {
+        throw new Error('Invalid data');
+      }
+      updateProjects(projects);
+      localStorage.setItem('projects', JSON.stringify(projects));
+    });
+});
